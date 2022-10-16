f33dd1c02cde8b6bfa632df2b539210b57a00cf0
unknown <nsromantsov@edu.hse.ru>
2022-10-17 02:29:15 +0300

    add ERC-20 token

---
+++ contracts/NiroToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract NiroToken is ERC20{
+    
+    uint constant _initial_supply = 100 * (10**18);
+
+    constructor() ERC20("NiroToken", "Niro") {
+        _mint(msg.sender, _initial_supply);
+    }
+}
